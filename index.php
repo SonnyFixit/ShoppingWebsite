7358561881f1516741b792cf75b99c208eb35330
SonnyFixit <fikser@tlen.pl>
2021-03-31 21:34:35 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,117 @@
+<?php 
+
+    $active='Home';
+    include("includes/header.php");
+
+?>
+   
+   
+   
+   <div id="advantages"><!-- #advantages Begin -->
+       
+       <div class="container"><!-- container Begin -->
+           
+           <div class="same-height-row"><!-- same-height-row Begin -->
+               
+               <div class="col-sm-4"><!-- col-sm-4 Begin -->
+                   
+                   <div class="box same-height"><!-- box same-height Begin -->
+                       
+                       <div class="icon"><!-- icon Begin -->
+                           
+                           <i class="fa fa-tag"></i>
+                           
+                       </div><!-- icon Finish -->
+                       
+                       <h3><a href="#">Best Prices</a></h3>
+                       
+                       <p>... </p>
+                       
+                   </div><!-- box same-height Finish -->
+                   
+               </div><!-- col-sm-4 Finish -->
+               
+               <div class="col-sm-4"><!-- col-sm-4 Begin -->
+                   
+                   <div class="box same-height"><!-- box same-height Begin -->
+                       
+                       <div class="icon"><!-- icon Begin -->
+                           
+                           <i class="fa fa-heart"></i>
+                           
+                       </div><!-- icon Finish -->
+                       
+                       <h3><a href="#">Best Products</a></h3>
+                       
+                       <p>....</p>
+                       
+                   </div><!-- box same-height Finish -->
+                   
+               </div><!-- col-sm-4 Finish -->
+               
+               <div class="col-sm-4"><!-- col-sm-4 Begin -->
+                   
+                   <div class="box same-height"><!-- box same-height Begin -->
+                       
+                       <div class="icon"><!-- icon Begin -->
+                           
+                           <i class="fa fa-star"></i>
+                           
+                       </div><!-- icon Finish -->
+                       
+                       <h3><a href="#">Best Quality</a></h3>
+                       
+                       <p>...</p>
+                       
+                   </div><!-- box same-height Finish -->
+                   
+               </div><!-- col-sm-4 Finish -->
+               
+           </div><!-- same-height-row Finish -->
+           
+       </div><!-- container Finish -->
+       
+   </div><!-- #advantages Finish -->
+   
+   <div id="hot"><!-- #hot Begin -->
+       
+       <div class="box"><!-- box Begin -->
+           
+           <div class="container"><!-- container Begin -->
+               
+               <div class="col-md-12"><!-- col-md-12 Begin -->
+                   
+                   <h2>
+                       Latest Offers
+                   </h2>
+                   
+               </div><!-- col-md-12 Finish -->
+               
+           </div><!-- container Finish -->
+           
+       </div><!-- box Finish -->
+       
+   </div><!-- #hot Finish -->
+   
+   <div id="content" class="container"><!-- container Begin -->
+       
+       <div class="row"><!-- row Begin -->
+          
+          <?php 
+           
+           getPro();
+           
+           ?>
+           
+       </div><!-- row Finish -->
+       
+   </div><!-- container Finish -->
+   
+   
+    
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+    
+    
+</body>
+</html>
